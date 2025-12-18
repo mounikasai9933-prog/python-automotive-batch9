4653465d333f9748b2d7a0b7124fdb25a47c1df3
mounikasai9933-prog <mounikasai9933@gmail.com>
2025-12-18 20:28:32 +0530

    commit

---
+++ range.py
@@ -0,0 +1,9 @@
+myList=["a","b","c"]
+x=len(myList)
+y=type(myList)
+print(x)
+print(y)
+
+x = range(5)
+for n in x:
+  print(n)
---
+++ tuple 2.py
@@ -0,0 +1,11 @@
+tup =()
+print(tup)
+
+tup = ('greeks','for')
+print(tup)
+  
+li = [1,2,4,5,6]
+print(tuple(li))
+
+tup = tuple('Greeks')
+print(tup)
